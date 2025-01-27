6102c0b60d66948c8d7d2cca58f86a4aa1b0f90a
A-CHERKAWI <abedelmouhsenc@gmail.com>
2025-01-27 12:21:57 -0500

    Initial commit

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,111 @@
 public class Book {
 
+
+
+    private String title;
+    private String author;
+    private long ISBN;
+    private double price;
+    private static int bookCount = 0;
+
+    public Book(String title, String author, long ISBN, double price) {
+        this.title = title;
+        this.author = author;
+        this.ISBN = ISBN;
+        this.price = price;
+        bookCount++;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public long getISBN() {
+        return ISBN;
+    }
+    public double getPrice() {
+        return price;
+    }
+
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    public void setISBN(long ISBN) {
+        this.ISBN = ISBN;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int findNumberOfCreatedBooks() {
+        return bookCount;
+    }
+
+
+    public boolean equals(Book otherBook) {
+        return (this.ISBN == otherBook.ISBN &&
+                this.price == otherBook.price);
+    }
+
+    @Override
+    public String toString() {
+        return "Book [title=" + title + ", author=" + author + ", ISBN=" + ISBN + ", price=" + price + "]";
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- src/MainDriver.java
+++ src/MainDriver.java
@@ -1,5 +1,110 @@
+import java.util.Scanner;
+
+// --------------------------------------------------------
+// Assignment #0
+// Question: (include question/part number, if applicable)
+// Written by: ABED-ELMOUHSEN CHERKAWI 40323359
+// --------------------------------------------------------
 public class MainDriver {
     public static void main(String[] args) {
 
+        double maxPrice;
+        int choice, innerChoice, maxBooks;
+        final String password = "249";
+        String  passwordAttempt;
+
+        Book[] inventory;
+        Scanner read = new Scanner(System.in);
+        System.out.println("Welcome to the COMP249 bookstore!");
+        System.out.println("Please enter the maximum number of books for the store: ");
+        maxBooks = read.nextInt();
+        inventory = new Book[maxBooks];
+
+
+        do {
+            System.out.print(" What do you want to do?" +
+                    "   \n1. Enter new books (password required) " +
+                    "   \n2. Change information of a book (password required) " +
+                    "   \n3. Display all books by a specific author " +
+                    "   \n4. Display all books under a certain a price. " +
+                    "   \n5. Quit " +
+                    " \nPlease enter your choice >");
+            choice = read.nextInt();
+
+            switch (choice) {
+                case 1:
+
+                    System.out.print("Enter password: ");
+                    for (int i = 1; i<4; i++) {
+                        passwordAttempt = read.nextLine();
+                        if (passwordAttempt.equals(password)) {
+                            System.out.println("Access granted");
+                            break;
+                        }
+                        else {
+                            System.out.println("Access denied, re-enter password, attempt: " + i + "/3");
+                        }
+
+                    }
+                break;
+
+                case 2:
+                    System.out.print("Enter password: ");
+                    passwordAttempt = read.nextLine();
+                    for (int i = 1; i<4; i++) {
+                        passwordAttempt = read.nextLine();
+                        if (passwordAttempt.equals(password)) {
+                            System.out.println("Access granted");
+                            break;
+                        }
+                        else {
+                            System.out.println("Access denied, re-enter password, attempt: " + i + "/3");
+                        }
+
+                    }
+
+
+                break;
+                case 3:
+
+
+
+
+                    break;
+
+
+
+                case 4:
+for (int k=0; k<maxBooks; k++) {
+    maxPrice = read.nextDouble();
+    if (inventory[k].getPrice()<maxPrice && inventory[k] != null) {};
+    System.out.println("Book #" + (k+1) +inventory[k].toString()+ "\n");
+}
+                 break;
+
+
+
+                case 5:
+                    System.out.println("Thank you for using COMP249 bookstore!");
+                break;
+
+            }
+
+
+
+
+
+        } while (choice != 5);
+
+
+
+
+
     }
+
+
+
+
+
+
 }
